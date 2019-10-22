b436a39136700fa5ad381bbdde586e669dee6264
Shota Tamura <tamura@yuuniworks.com>
2019-10-22 12:42:42 +0900

    simple express server

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const cors = require('cors');
+const morgan = require('morgan');
+var express = require('express');
+
+var app = express();
+
+app.use(bodyParser.json({ type: '*/*' })); // parses any requests into json
+app.use(cors());
+app.use(compression());
+app.use(morgan('combined'));
+
+const port = process.env.NODE_ENV === 'production' ? process.env.PORT : 8080;
+
+app.listen(port, () => {
+  console.log(`Server listening on: ${port}`);
+});
+
+app.get('/readiness', function(req, res, next) {
+  res.send('ok');
+});
