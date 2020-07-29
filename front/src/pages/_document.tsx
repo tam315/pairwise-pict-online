02244f4be6a65851e1d8663e0e1e54523407f4a1
Shota Tamura <tamura@yuuniworks.com>
2020-07-29 15:20:48 +0900

    add 'lang' attribute for seo

---
+++ front/src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      // This file was created just to specify this "lang" attribute
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
