f95c19f47fcf53c4dee221182ca92cf2d987d3ea
Shota Tamura <tamura@yuuniworks.com>
2020-07-30 15:29:44 +0900

    disable fetching js on server side

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -8,25 +8,31 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=UA-116967778-8"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];
+        {process.browser && (
+          // google analytics
+          <>
+            <script
+              async
+              src="https://www.googletagmanager.com/gtag/js?id=UA-116967778-8"
+            ></script>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', 'UA-116967778-8');`,
-          }}
-        ></script>
-        {/* adsens */}
-        <script
-          data-ad-client="ca-pub-7134126650568891"
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-        ></script>
-        {/* adsense end */}
+              }}
+            ></script>
+          </>
+        )}
+        {process.browser && (
+          // adsense
+          <script
+            data-ad-client="ca-pub-7134126650568891"
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          ></script>
+        )}
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Generate pairwise testcases online" />
